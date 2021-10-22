7e8d69a5d1ff5ef39dc9fbc90f0d549f519641b5
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-10-22 22:39:16 +0600

    file added

--- app/src/main/java/com/shajidurrahmansaad/miwok/NumbersActivity.java
+++ app/src/main/java/com/shajidurrahmansaad/miwok/NumbersActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -12,6 +16,23 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
 
