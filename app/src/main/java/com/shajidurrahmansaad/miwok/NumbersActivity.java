587581200021de6db77b42a4087eea8f32036069
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2021-10-23 12:32:26 +0600

    tem

--- app/src/main/java/com/shajidurrahmansaad/miwok/NumbersActivity.java
+++ app/src/main/java/com/shajidurrahmansaad/miwok/NumbersActivity.java
@@ -27,7 +27,7 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("six");
         words.add("seven");
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, R.layout.mylayout, words);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
